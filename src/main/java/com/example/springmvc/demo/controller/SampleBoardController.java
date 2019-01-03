63095541219d2765aaa1f680cff76fd852fbfdad
junhwan <developer.jangjh@gmail.com>
2019-01-03 23:26:19 +0900

    - pageVO 생성 - 페이징 처리 컨트롤러 수정

--- src/main/java/com/example/springmvc/demo/controller/SampleBoardController.java
+++ src/main/java/com/example/springmvc/demo/controller/SampleBoardController.java
@@ -1,17 +1,26 @@
 package com.example.springmvc.demo.controller;
 
+import com.example.springmvc.demo.vo.PageVO;
 import lombok.extern.java.Log;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/boards/")
 @Log
 public class SampleBoardController {
 
+//    @GetMapping("/list")
+//    public void list() {
+//        log.info("list() called");
+//    }
+
     @GetMapping("/list")
-    public void list() {
-        log.info("list() called");
+    public void list(PageVO pageVO) {
+        Pageable pageable = pageVO.makePageAble(0, "bno");
+        log.info("" + pageable);
     }
 }
