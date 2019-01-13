2bbefca783df9777e78c218f3812e52468ba4007
junhwan <developer.jangjh@gmail.com>
2019-01-13 22:13:15 +0900

    - 게시물 등록 기능 추가

--- src/main/java/com/example/springmvc/demo/controller/BoardController.java
+++ src/main/java/com/example/springmvc/demo/controller/BoardController.java
@@ -12,12 +12,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/boards/")
 @Log
-public class SampleBoardController {
+public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
@@ -32,5 +35,17 @@ public class SampleBoardController {
         Page<Board> boardPage = boardRepository.findAll(searchPredicate, pageable);
         model.addAttribute("result", boardPage);
         model.addAttribute("result2", new PageMakerVO(boardPage));
+    }
+
+    @GetMapping("/register")
+    public void registerGet(@ModelAttribute("vo")Board vo) {
+        log.info("registerGet");
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute("vo")Board vo, RedirectAttributes redirectAttributes) {
+        boardRepository.save(vo);
+        redirectAttributes.addFlashAttribute("msg", "success");
+        return "redirect:/boards/list";
     }
 }
